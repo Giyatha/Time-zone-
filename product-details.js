fdf726b2e77969882f25a85c4380b3a14a709735
Abdullah <abdulluhodulate@gmail.com>
2023-03-10 12:02:29 +0100

    welcome back

--- product-details.js
+++ product-details.js
@@ -1,5 +1,7 @@
 const buttons = document.querySelectorAll("[data-carousel-btn]");
-const nextBtn = document.querySelector('[data-carousel-btn="next"]');
+const nextBtn = document.querySelector('[data-carousel-button="next"]');
+const slideContainer = document.querySelector("[data-carousel] [data-slides]");
+let initialX = null;
 
 buttons.forEach((btn) => {
 	btn.addEventListener("click", () => {
@@ -20,3 +22,57 @@ buttons.forEach((btn) => {
 setInterval(() => {
 	nextBtn.click();
 }, 5000);
+
+slideContainer.addEventListener("touchstart", (e) => {
+	initialX = e.touches[0].clientX;
+});
+
+slideContainer.addEventListener("touchend", (e) => {
+	const finalX = e.changedTouches[0].clientX;
+	const deltaX = finalX - initialX;
+
+	let prevBtn, nextBtn;
+
+	prevBtn = document.querySelector('[data-carousel-button="prev"]');
+	nextBtn = document.querySelector('[data-carousel-button="next"]');
+
+	if (deltaX > 0) {
+		// console.log(prevBtn.click());
+		prevBtn.click();
+	} else if (deltaX < 0) {
+		// console.log(nextBtn.click);
+		nextBtn.click();
+	}
+});
+
+// AMOUNT FUNTIONALITY
+
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const amount = document.querySelector("#num");
+// console.log(plus);
+// console.log(amount);
+
+// const plusHandleClick = () => {
+// 	amount++;
+// 	console.log(amount);
+// };
+
+let num = 1;
+
+plus.addEventListener("click", () => {
+	if (num < 10) {
+		num++;
+		// console.log(num);
+		amount.innerText = num;
+		console.log(amount);
+	}
+});
+minus.addEventListener("click", () => {
+	if (num > 1) {
+		num--;
+		// console.log(num);
+		amount.innerText = num;
+		console.log(amount);
+	}
+});
